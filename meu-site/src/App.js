268e9b8c8aaba8c359117645d9c35325c15d8ba7
leandrolimadeveloper <leandroslimasr@gmail.com>
2022-01-14 17:58:29 -0300

    33. Entendendo props e component pt2

--- meu-site/src/App.js
+++ meu-site/src/App.js
@@ -1,10 +1,31 @@
 import './App.css';
 
-const Saudacao = (props) => {
+const Time = (props) => {
   return (
     <div>
-      <h2>Olá! tudo bem, {props.nome}</h2>
-      <p>Seja bem-vindo(a) ao curso de {props.curso}</p>
+      <Sobre username={props.nome} cargo={props.cargo}
+             idade={props.idade}/>
+      <Social fb={props.facebook}/>
+      <hr></hr>
+    </div>
+  )
+}
+
+const Sobre = (props) => {
+  return (
+    <div>
+      <h2>Olá! Eu sou o(a) {props.username}</h2>
+      <p>Cargo: {props.cargo}</p>
+      <p>Idade: {props.idade}</p>
+    </div>
+  )
+}
+
+const Social = (props) => {
+  return (
+    <div>
+      <a href={props.fb}>Facebook </a>
+      <a>Twitter</a>
     </div>
   )
 }
@@ -12,10 +33,10 @@ const Saudacao = (props) => {
 function App() {
   return (
     <div>
-      <p>Curso React</p>
-      <Saudacao nome="Leandro" curso="React Js"/>
-      <Saudacao nome="Lorena" curso="React Js"/>
-    </div>
+      <h1>Conheça o nosso time:</h1>
+      <Time nome="Leandro" cargo="Programador" idade="29" facebook="https://facebook.com"/>
+      <Time nome="José" cargo="Cozinheiro" idade="35" facebook="https://facebook.com"/>
+    </div> 
   )
 }
 
