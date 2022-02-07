12661a6e35518b0d2914f4266ef82cd7bd1b81e5
leandrolimadeveloper <leandroslimasr@gmail.com>
2022-02-07 18:01:35 -0300

    40. Renderização condicional

--- meu-site/src/App.js
+++ meu-site/src/App.js
@@ -1,11 +1,55 @@
 import React, {Component} from 'react'
-import Membro from './components/Membro' // Não precisa informar index.js
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      status: 1,
+      mensagem: true,
+      logado: false 
+    }
+
+    this.sair = this.sair.bind(this)
+    this.entrar = this.entrar.bind(this)
+  }
+
+  sair() {
+    this.setState({logado: false})
+  }
+
+  entrar() {
+    this.setState({logado: true})
+  }
+  
   render() {
     return(
       <div>
-        <Membro nome="Visitante"/>
+        {this.state.status === 1 &&
+          <h1>Bem-vindo(a) ao sistema</h1>
+        }
+
+        {this.state.mensagem ? 
+        <div>
+          <h2>Receba seu prêmio</h2>
+        </div> : 
+        <div>
+          <h2>Nenhum prêmio disponível no momento</h2>
+        </div>
+        }
+
+        
+        {this.state.logado ? 
+        <div>
+          <button onClick={this.sair}>Sair do sistema</button>
+        </div> : 
+        <div>
+          <button onClick={this.entrar}>Entrar no sistema</button>
+        </div>
+        }
+        
+        <div>
+          <h2>Curso React JS</h2>
+        </div>
       </div>
     )
   }
