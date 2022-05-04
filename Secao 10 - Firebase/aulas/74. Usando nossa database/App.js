abb72ba53f8914a7bcb1ed0d7ebf90dab8cdb71b
leandrolimadeveloper <leandroslimasr@gmail.com>
2022-05-03 21:36:30 -0300

    74. Usando nossa database

---
+++ Secao 10 - Firebase/aulas/74. Usando nossa database/App.js
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+import firebase from "./firebaseConnection";
+
+function App() {
+  const [carro, setCarro] = useState('')
+  const [montadora, setMontadora] = useState('')
+
+  async function handleAdd() {
+    await firebase.firestore().collection('carros')
+      .add({
+        modelo: carro,
+        montadora: montadora
+      })
+      .then(() => {
+        console.log('Dados cadastrado com sucesso!')
+        setCarro('')
+        setMontadora('')
+      })
+      .catch((error) => {
+        console.log('Erro: ' + error)
+      })
+  }
+
+  async function buscaCarro() {
+    await firebase.firestore().collection('carros')
+      .doc('23')
+      .get()
+      .then((snapshot) => {
+        setCarro(snapshot.data().modelo) // data.().campoDoBD
+        setMontadora(snapshot.data().montadora) 
+      })
+      .catch((err) => {
+        console.log('Erro: ' + err)
+      })
+  }
+
+  return (
+    <div className="App">
+      <h1>Reactjs e Firebase</h1>
+
+      <label>Carro: </label>
+      <input type="text" value={carro} onChange={(e) => setCarro(e.target.value)} />
+
+      <label>Montadora: </label>
+      <input type="text" value={montadora} onChange={(e) => setMontadora(e.target.value)} />
+      <br></br><br></br>
+      <button onClick={handleAdd}>Cadastrar</button>
+      <button onClick={buscaCarro}>Buscar carro</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ Secao 10 - Firebase/execucao/src/firebaseConnection.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDd5X7ZF3bhzIIEwvUFruN4MIEq7bcCawI",
+    authDomain: "cursoreact-ef0f9.firebaseapp.com",
+    projectId: "cursoreact-ef0f9",
+    storageBucket: "cursoreact-ef0f9.appspot.com",
+    messagingSenderId: "457973284723",
+    appId: "1:457973284723:web:9f902b14735bd53fe68f06",
+    measurementId: "G-ZEP560MFC4"
+};
+
+// Initialize Firebase
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig)
+}
+
+export default firebase
