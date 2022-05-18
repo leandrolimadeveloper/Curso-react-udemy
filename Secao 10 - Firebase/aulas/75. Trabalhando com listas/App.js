f0d8d9946fca156b55162b7babc5ff9b3b533272
leandrolimadeveloper <leandroslimasr@gmail.com>
2022-05-17 22:22:56 -0300

    75. Trabalhando com listas

---
+++ Secao 10 - Firebase/aulas/75. Trabalhando com listas/App.js
@@ -0,0 +1,89 @@
+import { useState } from 'react'
+import firebase from "./firebaseConnection";
+
+function App() {
+  const [carro, setCarro] = useState('')
+  const [montadora, setMontadora] = useState('')
+  const [carros, setCarros] = useState([])
+
+  async function handleAdd() {
+    await firebase.firestore().collection('carros')
+      .add({
+        modelo: carro,
+        montadora: montadora
+      })
+      .then(() => {
+        console.log('Dados cadastrado com sucesso!')
+        setCarro('')
+        setMontadora('')
+      })
+      .catch((error) => {
+        console.log('Erro: ' + error)
+      })
+  }
+
+  async function buscaCarro() {
+    await firebase.firestore().collection('carros')
+      .doc('23')
+      .get()
+      .then((snapshot) => {
+        setCarro(snapshot.data().modelo)
+        setMontadora(snapshot.data().montadora)
+      })
+      .catch((err) => {
+        console.log('Erro: ' + err)
+      })
+  }
+
+  async function mostraCarro() {
+    await firebase.firestore().collection('carros')
+      .get()
+      .then((snapshot) => {
+        let lista = []
+
+        snapshot.forEach((doc) => {
+          lista.push({
+            id: doc.id,
+            modelo: doc.data().modelo,
+            montadora: doc.data().montadora
+          })
+        })
+
+        setCarros(lista)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  return (
+    <div className="App">
+      <h1>Reactjs e Firebase</h1>
+
+      <label>Carro: </label>
+      <input type="text" value={carro} onChange={(e) => setCarro(e.target.value)} />
+
+      <label>Montadora: </label>
+      <input type="text" value={montadora} onChange={(e) => setMontadora(e.target.value)} />
+      <br></br><br></br>
+
+      <button onClick={handleAdd}>Cadastrar</button>
+      <button onClick={buscaCarro}>Buscar carro</button>
+      <button onClick={mostraCarro}>Mostrar carros cadastrados</button>
+
+      <ul>
+        {carros.map((carro) => {
+          return (
+            <li key={carro.id}>
+              <span>Carro: {carro.modelo} </span><br></br>
+              <span>Montadora: {carro.montadora} </span><br></br><br></br>
+            </li>
+          )
+        })}
+      </ul>
+
+    </div>
+  );
+}
+
+export default App;
